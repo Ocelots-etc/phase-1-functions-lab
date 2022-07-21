4056096a38b7fd4904ed0b55e992784f471cc381
catdgomez <catherinedgomez@gmail.com>
2022-07-20 20:10:31 -0500

    2 of 10 tests passing

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someNumber) {
+  return someNumber - 42
+}
+
+// function distanceFromHqInFeet() {
+
+//   let result
+
+
+
+//   return result 
+// }
+
+// function distanceTravelledInFeet() {
+
+//   let result
+
+
+
+//   return result
+// }
